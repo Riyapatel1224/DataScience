878018abca6d87be835667eda0df209e70b1afdb
Riyapatel1224 <riya.patell1204@gmail.com>
2024-01-26 23:59:50 +0530

    ...

---
+++ python/pythonCode/Functions/calArea.py
@@ -0,0 +1,8 @@
+# 1. Write a function called calculate_area that takes base and height as an input and returns and area of a triangle. Equation of an area of a triangle is 
+# area = (1/2)*base*height
+
+def calculate_area(base,height):
+    return (1/2)*base*height
+
+ans=calculate_area(10,5)
+print(ans)
