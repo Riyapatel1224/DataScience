10e3e2acde14bd4b182521042d21c197db862214
Riyapatel1224 <riya.patell1204@gmail.com>
2024-01-27 22:39:19 +0530

    ...

--- python/pythonCode/DictTuple/CountryRegion.py
+++ python/pythonCode/DictTuple/CountryRegion.py
@@ -8,7 +8,7 @@
 #    b. add: if user input add then it should further ask for a country name to add. If country already exist in our dataset then it should print that it exist and do nothing. If it doesn't then it asks for population and add that new country/population in our dictionary and print it
 #    c. remove: when user inputs remove it should ask for a country to remove. If country exist in our dictionary then remove it and print new dictionary using format shown above in (a). Else print that country doesn't exist!
 #    d. query: on this again ask user for which country he or she wants to query. When user inputs that country it will print population of that country.
-
+ 
 
 def printDict(population):
     for info in population:
