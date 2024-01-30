4493cd48373b386ca25282c6fe98fccfdad8aea5
Riyapatel1224 <riya.patell1204@gmail.com>
2024-01-30 17:55:58 +0530

    ...

--- python/pythonCode/FileObjects/practice/WriteFilesPractice.py
+++ python/pythonCode/FileObjects/practice/WriteFilesPractice.py
@@ -7,9 +7,17 @@
 #             wf.write(line) # copy text from test.txt to test_copy.txt
 
 
+# with open('FileObjects/practice/image.jpg','rb') as rf:
+#     with open('FileObjects/practice/image_copy.jpg','wb') as wf:
+#         for line in rf:
+#             wf.write(line)
+
+
 with open('FileObjects/practice/image.jpg','rb') as rf:
     with open('FileObjects/practice/image_copy.jpg','wb') as wf:
-        for line in rf:
-            wf.write(line)
-
-
+        chunk_size=20000
+        rf_chunk=rf.read(chunk_size)
+        while chunk_size > 0:
+            wf.write(rf_chunk)
+            rf_chunk=rf.read(chunk_size)
+            
